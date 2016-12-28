61e8ac8e8c1d48457a7304dd59d5b0216e20daf1
Piegefull13 <camille.liccia@etu.univ-amu.fr>
2016-12-28 19:52:02 +0100

    Déclaration Boutton Client

--- remote_client/remote_client.ino
+++ remote_client/remote_client.ino
@@ -5,6 +5,12 @@ const int redPin = 4;
 const int greenPin = 3;
 const int bluePin = 2;
 
+/* Broches pour les boutons */
+const byte PIN_BUTTON_A = 5;
+const byte PIN_BUTTON_B = 6;
+const byte PIN_BUTTON_C = 7;
+const byte PIN_BUTTON_D = 8;
+
 void setup() {
   Serial.begin(9600);
 }
