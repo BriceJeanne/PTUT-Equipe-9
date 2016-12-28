61dd0ca195b5f38d82e37f7600e1c82bbbc2f19b
Piegefull13 <camille.liccia@etu.univ-amu.fr>
2016-12-28 19:53:10 +0100

    Déclaration Message Serveur

--- remote_server/remote_server.ino
+++ remote_server/remote_server.ino
@@ -5,6 +5,12 @@ int redPin = 4;
 int greenPin = 3;
 int bluePin = 2;
 
+/* Différents messages de commande */
+const char* CMD_BUTTON_A = "BPA";
+const char* CMD_BUTTON_B = "BPB";
+const char* CMD_BUTTON_C = "BPC";
+const char* CMD_BUTTON_D = "BPD";
+
 void setup() {
   Serial.begin(9600);
 }
